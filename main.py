4e7018a2ca4a2f3280564f4ace26ec06fc43fcad
Demid1=FirstCode <funtik50funtik50@gmail.com>
2025-02-24 20:28:36 +0300

    Third commit

--- main.py
+++ main.py
@@ -1,54 +1,15 @@
-
 import math
-
+a = int(input("Введите , пожалуйста , число:"))
+b = input("Введите операцию:")
+c = int(input("Введите , пожалуйста , второе число:"))
+if b == '%':
+    print(a%c)
+if b == '**':
+    print(a**c)
+if b == '√':
+    print(math.sqrt(a))
 a = int(input())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def fac(n):
     if n == 1:
         return 1
     return fac(n - 1) * n
-
-
-
-
-
-
-
-
-
-
-
-
-
-
